c5dfbb551a50d2c71e5b73726aff5dd93f0200e9
Jacob-van-Almelo <jpvano@gmail.com>
2022-10-27 16:26:42 -1000

    practicing

---
+++ try.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 16:25:30 2022
+
+@author: jacobvanalmelo
+"""
+
+new script that doesn't work as an attempt to push and pull'
